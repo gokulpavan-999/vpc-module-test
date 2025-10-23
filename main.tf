6527e81861bb5a68e367ae9c5ef8c8cf59c490a2
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-23 18:11:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 module "vpc" {
   source = "git::https://github.com/gokulpavan-999/terraform-aws-vpc.git"
-  #VPC
+  # VPC
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
   environment = var.environment
   vpc_tags = var.vpc_tags
+
+  # public subnets
+  public_subnet_cidr = var.public_subnet_cidr
+
+  # private subnets
+  private_subnet_cidr = var.private_subnet_cidr
+
+  # database subnets
+  database_subnet_cidr = var.database_subnet_cidr
+
 }
 
 #data "aws_availability_zone" "available" {
